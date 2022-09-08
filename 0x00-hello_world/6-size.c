b580da5e65a6dd9a5c84650cff7dd9324288e3f3
The Gov <irepjoshua17@gmail.com>
2022-09-08 14:01:54 -0700

    Task 6 done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the size of various types 
+ * main - print the size of various types
  * Description: prints the size of various types on the computer it is compiled
  * and run on
  * Return: Always 0 (Success)
