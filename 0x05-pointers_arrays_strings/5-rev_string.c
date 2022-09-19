bd50f6966d5ca7b5356158f88a3dd3a9eadfec31
The Gov <irepjoshua17@gmail.com>
2022-09-19 08:55:08 -0700

    Task 5 done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,9 +19,11 @@ void rev_string(char *s)
 
 	for (i = 0; i < (count - 1); i++)
 	{
-												for (j = i + 1; j > 0; j--)
-		{											temp = *(str + j);
-			*(str + j) = *(str + (j - 1));							*(str + (j - 1)) = temp;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
 		}
 	}
 }
