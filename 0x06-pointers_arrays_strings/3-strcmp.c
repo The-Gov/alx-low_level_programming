ace57db6a2418614aaea1c7c6feea4c20c311f62
The Gov <irepjoshua17@gmail.com>
2022-09-22 13:30:19 -0700

    Corrected errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,29 @@
  **/
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i = 0, diff = 0;
 
-	while (op = 0)
+	while (1)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		op = *(s2 + 1) - *(a2 + 1);
-		i++;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return (op);
+	return (diff);
 }
